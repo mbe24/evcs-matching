59d5b60e5af6673bf10d549d463c44beab79b498
Mikael Beyene <mikael@beyene.org>
2019-02-26 14:32:13 +0100

    Fix timing in shutdown controller

--- webapp-common/src/main/java/org/beyene/webapp/common/controller/ShutdownController.java
+++ webapp-common/src/main/java/org/beyene/webapp/common/controller/ShutdownController.java
@@ -5,12 +5,17 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import java.util.Timer;
+import java.util.TimerTask;
 
+@CrossOrigin
+@ResponseBody
 @RestController
 public class ShutdownController {
 
@@ -21,13 +26,22 @@ public class ShutdownController {
 
     @PostConstruct
     void init() throws Exception {
-        logger.info("Shutdown controller initialized.");
+        logger.info("Shutdown controller initialized");
     }
 
     @PostMapping("/shutdown")
-    @ResponseBody
     public String shutdownContext() {
-        ((ConfigurableApplicationContext) context).close();
-        return "Shutting down...";
+        logger.info("Shutting down application");
+        new Timer().schedule(wrap(() -> ((ConfigurableApplicationContext) context).close()), 250);
+        return String.format("Shutting down...%n");
+    }
+
+    private static TimerTask wrap(Runnable r) {
+        return new TimerTask() {
+            @Override
+            public void run() {
+                r.run();
+            }
+        };
     }
 }
